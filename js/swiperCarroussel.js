07b860478db65bc0fabbe0027e211a317bc09bbc
Gabriel Nunes de Almeida <gabrielnalmeida20@gmail.com>
2023-10-17 08:26:26 -0300

    feat: carrossel addition made with swiper.js

---
+++ js/swiperCarroussel.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: true,
+      pauseOnMouseEnter: true
+    },
+    slidesPerView: 2,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 3,
+      slideShadows: false
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
